2a1498b7ea371255c8a683593b0e0d559e4e2cf8
Mauricio <mcb75851@udelasalle.edu.mx>
2024-09-18 19:02:49 -0600

    Fixes

--- monkeypox-api/src/routes/caseRoutes.ts
+++ monkeypox-api/src/routes/caseRoutes.ts
@@ -7,6 +7,6 @@ router.post('/cases', createCase); //crear un caso
 router.get('/cases', getAllCases); //obtener todos los casos
 router.get('/cases/recent', getRecentCases); //obtener los casos de la ultima semana
 router.put('/cases/:id', updateCase); //Actualizar un caso
-router.delete('/cases/:id', deleteCase); //Eliminar un caso
+router.delete('/cases/:id', deleteCase); //Eliminar un caso para que no exista
 
 export default router;
